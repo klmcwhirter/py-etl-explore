98d13730a160683db938f6b68320050ec0b66823
Kevin McWhirter <klmcwhirter@gmail.com>
2020-06-02 08:41:45 -0700

    bonobo_etl examples are now working

--- bonobo_etl.py
+++ bonobo_etl.py
@@ -1,51 +1,118 @@
+"""a simple ETL modulle
 
-from bonobo.config import use, requires
+Usage:
+    bonobo_etl.py load [--conn=<conn-string>] [--file=<file>] [-v | --verbose]
+
+Options:
+  --conn=<conn-string>  The connection string to use. e.g., sqlite:///pandas_etl.db [default: sqlite:///:memory:]
+  --file=<file>         The input file to process [default: KPHX_with_reserved.dat]
+  -v, --verbose         More verbose output [defaut: false]
+  -h --help             Show this screen.
+  --version             Show version.
+
+"""
+
+import csv
+from datetime import datetime
+
 import bonobo
+from bonobo.config import use
+from docopt import docopt
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models.weather import Weather
+
+# TODO move this line somewhere central ...
+csv.register_dialect('pipe_delim', delimiter='|')
 
 
 def get_services(**kwargs):
+    """ provide services that can be injected """
+
+    verbose = kwargs['--verbose'] if kwargs['--verbose'] else False
+
+    if verbose:
+        print(kwargs)
+
     return {
-        'datareader': bonobo.CsvReader(
-            kwargs['filename'],
-            delimiter='|',
-            fields=['id', 'city', 'date', 'actual_mean_temp', 'actual_min_temp', 'actual_max_temp',
-                    'actual_precipitation', 'average_precipitation', 'record_precipitation', 'reserved2']
-        ),
-        'filename': kwargs['filename']
+        **kwargs,
+        'verbose': verbose,
+        'filename': kwargs['--file'],
+        'fieldnames': ['id', 'city', 'date',
+                       'actual_mean_temp', 'actual_min_temp', 'actual_max_temp',
+                       'actual_precipitation', 'average_precipitation',
+                       'record_precipitation', 'reserved2'],
+        'sqlalchemy.engine': create_engine(kwargs['--conn'], echo=verbose),
     }
 
 
-def arg0_to_kwargs(**row):
-    return {**row}
-
 @use('filename')
-@use('datareader')
-def read_recs(filename, datareader):
-    print(type(datareader))
-    for rec in datareader.read(filename):
-        yield rec
+@use('fieldnames')
+@use('verbose')
+def read_recs(filename, fieldnames, verbose):
+    """ read recs using streaming protocol """
+    with open(filename, newline='') as f:
+        reader = csv.DictReader(f, dialect='pipe_delim', fieldnames=fieldnames)
+        next(reader, None)  # skip header row
+        for row in reader:
+            if verbose:
+                print(f'read_recs: row={row}')
 
-def transform(**row):
-    print(f'transform ...')
-    return {**row}
+            del row[None]  # remove unused fields
 
-def write_recs(**row):
-    print(f'write_recs: {row}')
+            weatherrec = Weather(**row)  # use kwargs expansion of the dict
 
-graph = bonobo.Graph(
-    bonobo.CsvReader(
-        'KPHX.dat',
-        delimiter='|',
-        fields=['id', 'city', 'date', 'actual_mean_temp', 'actual_min_temp', 'actual_max_temp',
-                'actual_precipitation', 'average_precipitation', 'record_precipitation', 'reserved2']
-    ),
-    arg0_to_kwargs,
-    transform,
-    write_recs)
+            weatherrec.date = datetime.strptime(row['date'], '%Y-%m-%d')
 
-bonobo.run(graph, services=get_services(filename='KPHX.dat'))
+            if verbose:
+                print(f'read_recs: weatherrec={weatherrec}')
 
-graph.add_chain(
-    bonobo.PrettyPrinter(),
-    _input=arg0_to_kwargs
-)
+            yield weatherrec
+
+
+@use('session')
+@use('verbose')
+def write_recs(rec, session, verbose):
+    """ write recs as provided """
+    if verbose:
+        print(f'write_recs: rec={rec}')
+
+    session.add(rec)
+
+
+def main(args):
+    """ main program """
+
+    graph = bonobo.Graph(
+        read_recs,
+        write_recs
+    )
+
+    # This is not that useful, but does show how to create more complicated graphs
+    # graph.add_chain(
+    #     bonobo.PrettyPrinter(),
+    #     _input=read_recs
+    # )
+
+    services = get_services(**args)
+
+    engine = services['sqlalchemy.engine']
+
+    # We do not want to do this in production - it creates the tables ...
+    Weather.metadata.create_all(engine)
+
+    # Make a session
+    session = sessionmaker(bind=engine)()
+
+    # Add it to injectable services
+    services['session'] = session
+
+    bonobo.run(graph, services=services)
+
+    session.commit()
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='1.0')
+    main(arguments)
--- pandas_etl.py
+++ pandas_etl.py
@@ -1,10 +1,11 @@
 """a simple ETL modulle
 
 Usage:
-    pandas_etl.py load [--conn=<conn-string>] [-v | --verbose]
+    pandas_etl.py load [--conn=<conn-string>] [--file=<file>] [-v | --verbose]
 
 Options:
   --conn=<conn-string>  The connection string to use. e.g., sqlite:///pandas_etl.db [default: sqlite:///:memory:]
+  --file=<file>         The file to process [default: KPHX_with_reserved.dat]
   -v, --verbose         More verbose output [defaut: false]
   -h --help             Show this screen.
   --version             Show version.
@@ -16,58 +17,30 @@ import logging
 from docopt import docopt
 
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Date, Float, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 import pandas as pd
 
-# Declare a Base from which concrete classes should extend.
-Base = declarative_base()
+from models.weather import Base, Weather
 
-class Weather(Base):
-    ''' the weather class 
-    
-        Define a class that provides the OR/M - mapping
-    '''
-    __tablename__ = 'weather'
-
-    id = Column(Integer, primary_key=True)
-
-    city = Column(String)
-    date = Column(Date)
-    actual_mean_temp = Column(Integer)
-    actual_min_temp = Column(Integer)
-    actual_max_temp = Column(Integer)
-
-    actual_precipitation = Column(Float)
-    average_precipitation = Column(Float)
-    record_precipitation = Column(Float)
-
-    reserved2 = Column(String)
-
-    def __repr__(self):
-        return f'city={self.city}, date={self.date}'
-
-
-def main(arguments):
+def main(args):
     """The main program"""
 
-    verbose = arguments['--verbose'] if arguments['--verbose'] else False
+    verbose = args['--verbose'] if args['--verbose'] else False
 
     llevel = logging.DEBUG if verbose else logging.INFO
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=llevel)
 
     logging.basicConfig(format='', level=logging.INFO)
 
-    if(verbose):
-        logging.debug(arguments)
+    if verbose:
+        logging.debug(args)
 
-    conn_string = arguments['--conn']
+    conn_string = args['--conn']
     logging.info(f'setting conn_string to "{conn_string}""')
 
     # Populate from file
-    df_with_reserved = pd.read_table('KPHX_with_reserved.dat', sep='|', header=0,
+    df_with_reserved = pd.read_table(args['--file'], sep='|', header=0,
                                      usecols=[
                                          'id', 'city', 'date', 'actual_mean_temp', 'actual_min_temp', 'actual_max_temp', 'actual_precipitation', 'average_precipitation', 'record_precipitation', 'reserved2'
                                      ],
@@ -93,14 +66,13 @@ def main(arguments):
     Base.metadata.create_all(engine)
 
     # Make a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = sessionmaker(bind=engine)()
 
-    for index, row in df_with_reserved.iterrows():
-        w = Weather(**row)
-        if(verbose):
-            print(w)
-        session.add(w)
+    for _, row in df_with_reserved.iterrows():
+        weather = Weather(**row)
+        if verbose:
+            print(weather)
+        session.add(weather)
 
     session.commit()
 
