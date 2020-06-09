8698dd17271b8c66f1c0ed930d37b92e98aaa784
Kevin McWhirter <klmcwhirter@gmail.com>
2020-06-08 22:46:51 -0700

    Refactor API options until AppContext can be put into place.

--- app.py
+++ app.py
@@ -12,22 +12,33 @@ logging.basicConfig(stream=sys.stdout,
 app = Flask(__name__)
 
 
-@app.route('/etl/<command>/<path:options>')
-def do_etl(command, **options):
+@app.route('/etl/<command>/<verbose>')
+def do_etl(command, *args, **kwargs):
     """ accept GET request and execute command with the options passed """
 
     try:
-        logging.debug('options=%s', options)
-        _opts = dict(options)
+        logging.debug('kwargs=%s', kwargs)
+        _opts = api_kwargs_to_options(**kwargs)
         logging.info('Executing %s with options %s', command, _opts)
 
         cmd = get_command(command)
 
         logging.debug('Found %s', cmd)
-        print(cmd.__module__.__doc__)
 
         cmd(_opts)
     except Exception as ex:
         return {"status": "err", "msg": repr(ex)}
 
     return {"status": "ok"}
+
+
+def api_kwargs_to_options(**kwargs):
+    """ convert kwargs to options """
+
+    verbose = True if 'verbose' in kwargs and kwargs['verbose'] != 'False' else False
+
+    return {
+        '--verbose': verbose,
+        '--conn': 'sqlite:///:memory:',
+        '--file': 'data/inbound/KPHX_with_reserved.dat'
+    }
