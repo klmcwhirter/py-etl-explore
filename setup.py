0db9136975bde95f1f697f15126c5a6d44202964
Kevin McWhirter <klmcwhirter@gmail.com>
2020-06-09 07:06:46 -0700

    Fix typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="py-etl-explore",
-    decription="exploration project of ETL with Python",
+    description="exploration project of ETL with Python",
     version="0.1",
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'}
